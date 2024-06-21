4f9e411e0a680d0bc2db74c87e322feaa3500911
Zach Smith <z@xmyth.me>
2024-06-20 22:30:32 -0700

    Change indentation for tempalte

--- src/main.rs
+++ src/main.rs
@@ -52,53 +52,55 @@ fn try_get_output_writer(output_file_name: &PathBuf) -> Result<Box<dyn Write>, E
 }
 
 fn create_note_entry(note: &MandolinNote) -> String {
-    let note_name: &String = &note.name;
-    let clef: &String = match &note.clef {
-      Some(c) => c,
-      _ => &String::from("treble")
-    };
-    let note_value: &String = &note.value;
-    let note_name_as_accidental: &String = &note_name.replace("b", "\\super \\flat").replace("#", "\\super \\sharp");
-    let fret_diagram: &String = &note.frets.iter().fold(String::new(), |frets: String, f: &String| frets + f + ";");
-    let frets_to_string_names: &Vec<String> = &note.frets.iter().map(|f| { f.replace("4-", "G-").replace("3-", "D-").replace("2-", "A-").replace("1-", "E-") }).collect();
-    let fret_names: &String = &frets_to_string_names.join(", ");
+  const DEFAULT_CLEF: &str = "treble";
 
-    return format!(r#"
-      \book {{
-        \bookOutputName "{note_name}"
-        \bookOutputSuffix "front"
-        \score {{
-          \new Staff {{
-            \omit Staff.BarLine
-            \omit Staff.TimeSignature
-            \clef "{clef}"
-            \new Voice {{ 
-              {{ {note_value} }}
-            }}
+  let note_name: &String = &note.name;
+  let clef: &String = match &note.clef {
+    Some(c) => c,
+    _ => &DEFAULT_CLEF.to_string()
+  };
+  let note_value: &String = &note.value;
+  let note_name_as_accidental: &String = &note_name.replace("b", "\\super \\flat").replace("#", "\\super \\sharp");
+  let fret_diagram: &String = &note.frets.iter().fold(String::new(), |frets: String, f: &String| frets + f + ";");
+  let frets_to_string_names: &Vec<String> = &note.frets.iter().map(|f| { f.replace("4-", "G-").replace("3-", "D-").replace("2-", "A-").replace("1-", "E-") }).collect();
+  let fret_names: &String = &frets_to_string_names.join(", ");
+
+  return format!(r#"
+    \book {{
+      \bookOutputName "{note_name}"
+      \bookOutputSuffix "front"
+      \score {{
+        \new Staff {{
+          \omit Staff.BarLine
+          \omit Staff.TimeSignature
+          \clef "{clef}"
+          \new Voice {{
+            {{ {note_value} }}
           }}
         }}
       }}
+    }}
 
-      \book {{
-        \bookOutputName "{note_name}"
-        \bookOutputSuffix "back"
-        \markup {{
-          \center-column {{
-            % Note Name
-            \concat {{ {note_name_as_accidental} }}
-            \override #'(fret-diagram-details . (
-                        (string-count . 4)
-                        (number-type . arabic)
-                        (fret-count . 20)
-                        (open-string . '0')
-                        (dot-color . red)
-            ))
-            % Fret Diagram
-            \fret-diagram "{fret_diagram}"
-            % Fret Names
-            \fontsize #-5 "{fret_names}"
-          }}
+    \book {{
+      \bookOutputName "{note_name}"
+      \bookOutputSuffix "back"
+      \markup {{
+        \center-column {{
+          % Note Name
+          \concat {{ {note_name_as_accidental} }}
+          \override #'(fret-diagram-details . (
+                      (string-count . 4)
+                      (number-type . arabic)
+                      (fret-count . 20)
+                      (open-string . '0')
+                      (dot-color . red)
+          ))
+          % Fret Diagram
+          \fret-diagram "{fret_diagram}"
+          % Fret Names
+          \fontsize #-5 "{fret_names}"
         }}
       }}
-    "#);
+    }}
+  "#);
 }
